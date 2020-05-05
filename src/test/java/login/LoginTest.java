217c9153e8c7791fc294a5269a02dd85f0c145e7
ihtishaam.ulhassan <ihtishaamulhassanqa@gmail.com>
2020-05-04 22:12:01 -0400

    I made a Page Factory

--- src/test/java/login/LoginTest.java
+++ src/test/java/login/LoginTest.java
@@ -1,4 +1,23 @@
 package login;
 
-public class LoginTest {
+import base.ScriptBase;
+import org.testng.annotations.BeforeTest;
+import pagecontroller.LogInController;
+import org.testng.annotations.Test;
+
+public class LoginTest extends ScriptBase {
+
+    LogInController login;
+    @BeforeTest
+    public void Init(){
+        Beforetest();
+
+    }
+
+    @Test
+    public void verifySignInButton(){
+        login=new LogInController(driver);
+        login.SignInTab();
+
+    }
 }
