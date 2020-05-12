5ebf72aab01286f2ad11116bdaa312e9bdc02607
ihtishaam.ulhassan <ihtishaamulhassanqa@gmail.com>
2020-05-11 21:46:21 -0400

    I added an xml file, parameterize

--- src/main/java/base/ScriptBase.java
+++ src/main/java/base/ScriptBase.java
@@ -2,18 +2,25 @@ package base;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class ScriptBase {
     public WebDriver driver;
-
-    public void Beforetest(){
+    @Parameters("browser")
+    @BeforeClass
+    public void Beforetest(String browser){
+        if (browser.equalsIgnoreCase("chrome")){
         System.setProperty("webdriver.chrome.driver","./drivers/chromedriver");
         driver=new ChromeDriver();
+        }
         driver.get("http://automationpractice.com/index.php");
-
-
-
+    }
+    @AfterClass
+    public void closeBrowser(){
+        driver.quit();
 
     }
 }
